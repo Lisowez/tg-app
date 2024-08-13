3675b9442258f612dc0b99439ec19403aa4ea0de
Lisowez <lisowez@icloud.com>
2024-08-13 21:32:26 +0300

    fix: fix bug

--- telegram-app/src/utils/routing.tsx
+++ telegram-app/src/utils/routing.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import { FullItem } from "../pages/FullItem/FullItem";
 
+
 export const router = createBrowserRouter([
   {
     path: "/",
